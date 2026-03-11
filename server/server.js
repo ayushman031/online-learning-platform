07a832a72d3e0c2ef3185ca0394fd8c9265a2699
ayushman031 <ayushmannkaushal@gmail.com>
2026-03-11 22:55:25 +0530

    Fix Express 5 wildcard routing using middleware

--- server/server.js
+++ server/server.js
@@ -24,7 +24,8 @@ const path = require('path');
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
-    app.get('(.*)', (req, res) => {
+    // Catch-all route for SPA fallback
+    app.use((req, res) => {
         res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
     });
 } else {
